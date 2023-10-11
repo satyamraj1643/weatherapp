4346b59a0525f55465d6185d67f34df6c6bb72b3
Satyamraj1532005 <satyamraj1643@gmail.com>
2023-10-11 20:33:33 +0530

    Added feature to fetch weather automatically without inputting a city name

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const apikey = "a442f4114f454ae28d583752231010"
 
+const auto = document.getElementById('get-location')
 const button = document.getElementById("searchbutton");
 const city = document.getElementById("cityInput");
 
@@ -22,20 +23,58 @@ async function weather(cityName){
 
     return await promise.json();
 }
+
+function  fetchWeatherhelper(event){
+    fetchWeather(event)
+}
+
+
+const fetchWeather =  async (event)=>{
+      const cityLongitude = event.coords.longitude;
+      const cityLatitude = event.coords.latitude
+     // console.log(cityLatitude, cityLongitude);
+     
+      
+      const promise = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=a442f4114f454ae28d583752231010&q=${cityLatitude}, ${cityLongitude} &aqi=no`
+      )
+      const details = await promise.json();
+      renderDetails(details);
+}
+
+function someError(){
+    console.log("Some error Ocurred, please try again or you might have not given location acess.")
+}
+
+auto.addEventListener('click', ()=>{
+   const result = navigator.geolocation.getCurrentPosition((fetchWeatherhelper), (someError))
+})
+
+
+
+
 button.addEventListener('click', async (event)=>{
     console.log(city.value);
     const value = city.value;
     const result = await weather(value);
+   
     
     console.log(result);
     console.log(result.current.condition.text)
 
+    renderDetails(result)
+
+   
+})
+
+const renderDetails = (result)=>{
     cityname.innerText =` ${result.location.name}, ${result.location.region} - ${result.location.country}`
     temp.innerText  = `${result.current.feelslike_c}`;
     realfeel.innerText= `${result.current.condition.text}`
     dateandtime.innerText = `${result.location.localtime.split(' ')[1]}`
    
-})
+
+}
 
 
 
